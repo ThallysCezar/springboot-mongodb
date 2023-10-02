1749caed13af169ad5f9cea71ff4a6acdfb7025c
Thallys Cezar <thalys_cezar@hotmail.com>
2023-10-02 11:11:09 -0300

    refactor: add exceptions handling findById

--- src/main/java/com/spring/mongodb/resources/UserResource.java
+++ src/main/java/com/spring/mongodb/resources/UserResource.java
@@ -1,14 +1,17 @@
 package com.spring.mongodb.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.spring.mongodb.domain.User;
+import com.spring.mongodb.dtos.UserDTO;
 import com.spring.mongodb.services.UserService;
 
 @RestController
@@ -20,8 +23,15 @@ public class UserResource {
 	
 	
 	@GetMapping
-	public ResponseEntity<List<User>> findAll(){
+	public ResponseEntity<List<UserDTO>> findAll(){
 		List<User> list = userService.findAll();
-		return ResponseEntity.ok().body(list);
+		List<UserDTO> listDTO = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
+	}
+	
+	@GetMapping("{id}")
+	public ResponseEntity<UserDTO> findById(@PathVariable String id){
+		User obj = userService.findById(id);
+		return ResponseEntity.ok().body(new UserDTO(obj));
 	}
 }
