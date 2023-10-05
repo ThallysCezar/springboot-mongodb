da96153c287fe373a88f828020aee731b249da7b
Thallys Cezar <thalys_cezar@hotmail.com>
2023-10-05 09:54:30 -0300

    refactor: add fullsearch in the postResource

--- src/main/java/com/spring/mongodb/services/PostService.java
+++ src/main/java/com/spring/mongodb/services/PostService.java
@@ -1,5 +1,7 @@
 package com.spring.mongodb.services;
 
+import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,5 +20,18 @@ public class PostService {
 	public Post findById(String id) {
 		Optional<Post> obj = postRepository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+//	public List<Post> findByTitle(String text){
+//		return postRepository.findByTitleContainingIgnoreCase(text);
+//	}
+	
+	public List<Post> findByTitle(String text){
+		return postRepository.searchTitle(text);
+	}
+	
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate){
+		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+		return postRepository.fullSearch(text, minDate, maxDate);
 	}
 }
